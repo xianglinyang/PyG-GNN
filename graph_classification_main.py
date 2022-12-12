9f1136e4e086311258d9280d6f2f2cb436988eeb
xianglin yang <yxlyzxl@gmail.com>
2022-12-12 19:20:56 +0800

    graph classification

---
+++ graph_classification_main.py
@@ -0,0 +1,90 @@
+import os
+import torch
+os.environ['TORCH'] = torch.__version__
+print(torch.__version__)
+
+from torch_geometric.datasets import TUDataset
+
+from models import gcn
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", "-d", choices=["MUTAG", "ENZYMES", "PROTEINS", "COLLAB", "IMDB-BINARY", "REDDIT-BINARY"])
+args = parser.parse_args()
+
+dataset = TUDataset(root='data/TUDataset', name='{}'.format(args.dataset))
+
+print()
+print(f'Dataset: {dataset}:')
+print('====================')
+print(f'Number of graphs: {len(dataset)}')
+print(f'Number of features: {dataset.num_features}')
+print(f'Number of classes: {dataset.num_classes}')
+
+data = dataset[0]  # Get the first graph object.
+
+print()
+print(data)
+print('=============================================================')
+
+# Gather some statistics about the first graph.
+print(f'Number of nodes: {data.num_nodes}')
+print(f'Number of edges: {data.num_edges}')
+print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+print(f'Has isolated nodes: {data.has_isolated_nodes()}')
+print(f'Has self-loops: {data.has_self_loops()}')
+print(f'Is undirected: {data.is_undirected()}')
+
+torch.manual_seed(12345)
+dataset = dataset.shuffle()
+
+train_dataset = dataset[:150]
+test_dataset = dataset[150:]
+
+print(f'Number of training graphs: {len(train_dataset)}')
+print(f'Number of test graphs: {len(test_dataset)}')
+
+
+from torch_geometric.loader import DataLoader
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+for step, data in enumerate(train_loader):
+    print(f'Step {step + 1}:')
+    print('=======')
+    print(f'Number of graphs in the current batch: {data.num_graphs}')
+    print(data)
+    print()
+
+model = gcn.GCN(num_features=dataset.num_features, hidden_channels=64, num_classes=dataset.num_classes)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+criterion = torch.nn.CrossEntropyLoss()
+
+def train():
+    model.train()
+
+    for data in train_loader:  # Iterate in batches over the training dataset.
+         out = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
+         loss = criterion(out, data.y)  # Compute the loss.
+         loss.backward()  # Derive gradients.
+         optimizer.step()  # Update parameters based on gradients.
+         optimizer.zero_grad()  # Clear gradients.
+
+def test(loader):
+     model.eval()
+
+     correct = 0
+     for data in loader:  # Iterate in batches over the training/test dataset.
+         out = model(data.x, data.edge_index, data.batch)  
+         pred = out.argmax(dim=1)  # Use the class with highest probability.
+         correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+     return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+
+
+for epoch in range(1, 171):
+    train()
+    train_acc = test(train_loader)
+    test_acc = test(test_loader)
+    print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
---
+++ models/gnn.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear
+from torch_geometric.nn import GraphConv, global_mean_pool
+
+
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GNN, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = ...  # TODO
+        self.conv2 = ...  # TODO
+        self.conv3 = ...  # TODO
+        self.lin = Linear(hidden_channels, dataset.num_classes)
+
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        x = global_mean_pool(x, batch)
+
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        
+        return x
